5fc2953e37561f2515aa92691fbc782361d84605
Wessel <wessel@autumnskies-online.co.za>
2025-05-15 21:39:06 +0200

    Initial BroCourse commit: Clean JavaFX project setup with Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,8 +16,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, Color.GREY);
+
+        Image icon = new Image("as_logo.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("ASO Calculator");
+
 
         stage.setScene(scene);
         stage.show();
