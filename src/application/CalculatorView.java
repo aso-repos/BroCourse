807576f4097318f055f4d9e8985bc4184eb64111
Wessel <wessel@autumnskies-online.co.za>
2025-05-24 18:46:08 +0200

    First lecture completed with code and image added

---
+++ src/application/CalculatorView.java
@@ -0,0 +1,21 @@
+package application;
+import javafx.event.ActionEvent;
+
+public class CalculatorView {
+
+    public void up (ActionEvent e) {
+        System.out.println("UP");
+    }
+
+    public void down (ActionEvent e) {
+        System.out.println("DOWN");
+    }
+
+    public void left (ActionEvent e) {
+        System.out.println("LEFT");
+    }
+
+    public void right (ActionEvent e) {
+        System.out.println("RIGHT");
+    }
+}
